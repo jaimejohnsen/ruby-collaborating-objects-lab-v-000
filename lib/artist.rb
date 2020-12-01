fe6ab331b0eef79c8e687a1ef3a8dfe30a906894
Jaime Johnsen <drivetraindiva@gmail.com>
2020-12-01 17:57:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -32,9 +32,9 @@ class Artist #class is the blueprint from which objects are created
     self.find_by_name(name) || self.create_by_name(name) #Find the artist instance that has that name or create one if it doesn't exist
   end #returns an instance of an artist with the name attribute
 
-  def print_songs
+  #def print_songs
 
-  end
+#end
 
 end
 
